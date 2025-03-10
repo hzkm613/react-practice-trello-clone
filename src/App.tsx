af153a6857a84c0e58a0bbb433157bcfb50ae0b4
hzkm613 <hzkm613@gmail.com>
2025-03-10 15:43:36 -0400

    Create draggable cards in board

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from 'styled-components'; 
+import {DragDropContext, Droppable, Draggable, DropResult} from 'react-beautiful-dnd';
+import { useRecoilState } from 'recoil';
+import { toDoState } from './atoms';
+import DraggableCard from './Components/DraggableCard';
+
+const Wrapper = styled.div`
+  width: 100vh;
+  height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Boards = styled.div`
+  width: 100%;
+  min-width: 250px;
+  max-width: 480px;
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+`;
+
+const Board = styled.div`
+ background-color: ${(props) => props.theme.boardColor};
+ padding: 30px 10px 20px 10px;
+ border-radius: 5px;
+ min-height: 200px;
+`;
+
+
+
+const toDos =  ["a", "b", "c", "d", "e", "f"];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const onDragEnd = ({ draggableId, destination, source}: DropResult) => {
+    // User drops it on the same place
+    if(!destination) return;
+    setToDos((currentToDos) => {
+      const toDosCopy = [...currentToDos];
+      // 1. Delete item on source.index
+      toDosCopy.splice(source.index, 1);
+      // 2. Put back the item on the destination.index
+      toDosCopy.splice(destination?.index, 0, draggableId);
+      return toDosCopy;
+    });
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Wrapper>
+        <Boards>
+          <Droppable droppableId='one'>
+            {(provided) => (
+              <Board 
+                ref={provided.innerRef} 
+                {...provided.droppableProps}
+              >
+                {toDos.map((toDo, index) => (
+                  <DraggableCard key={toDo} index={index} toDo={toDo} />
+                ))}
+                {provided.placeholder}
+              </Board>
+            )}
+          </Droppable>
+        </Boards>
+      </Wrapper>
+    </DragDropContext>
+  );
 }
 
 export default App
---
+++ src/Components/DraggableCard.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Draggable } from 'react-beautiful-dnd'
+import styled from 'styled-components';
+
+const Card = styled.div`
+  border-radius: 5px;
+  padding: 10px 10px;
+  background-color: ${(props) => props.theme.cardColor};
+  margin-bottom: 5px;
+`;
+
+interface IDraggableCard {
+    toDo: string;
+    index: number;
+}
+
+const DraggableCard = ({toDo, index}: IDraggableCard) => {
+  return (
+<Draggable draggableId={toDo} key={toDo} index={index}>
+                    {(provided) => (
+                      <Card 
+                        ref={provided.innerRef} 
+                        {...provided.draggableProps} 
+                        {...provided.dragHandleProps}
+                      >
+                        {toDo}
+                      </Card>
+                    )}
+                  </Draggable>
+  )
+}
+
+export default DraggableCard
